b21d185382d9773b732160564b9f57345c9d761e
Christian Wißmann <christian.wissmann@saxsys.de>
2016-02-26 13:02:41 +0100

    update storage-table (add person)

--- azure-example/storage-table/Program.cs
+++ azure-example/storage-table/Program.cs
@@ -11,25 +11,45 @@ namespace storage_table
 {
     class Program
     {
-        private static string accountName = "";
-        private static string keyValue = "";
+        private static string accountName = "saxsysdojotest";
+        private static string keyValue = "R1a0JZi11ZZ9sWSy/PX8wazBV+SFK/7Xru0LdkIeCTV9mLxfBkij3ks72IOYM/lJ0g/kKSgwlfo5+tFrrtzILg==";
 
         private static IList<Auto> autos = new List<Auto>() {
-            new Auto("Opel", "Astra") { Farbe = "blau", PS = 120 },
-            new Auto("VW", "Golf") { Farbe = "rot", PS = 85 },
-            new Auto("VW", "Passat") { Farbe = "silber", PS = 140 },
-            new Auto("Opel", "Zafira") { Farbe = "rot", PS = 140 },
-            new Auto("BMW", "3er") { Farbe = "weiß", PS = 200 },
-            new Auto("Mercedes", "E-Klasse") { Farbe = "schwarz", PS = 170 }
+            new Auto("autos", Guid.NewGuid()) { Marke = "Opel", Modell = "Astra", Farbe = "blau", PS = 120 },
+            new Auto("autos", Guid.NewGuid()) { Marke = "VW", Modell = "Passat", Farbe = "rot", PS = 85 },
+            new Auto("autos", Guid.NewGuid()) { Marke = "VW", Modell = "Golf", Farbe = "silber", PS = 140 },
+            new Auto("autos", Guid.NewGuid()) { Marke = "Opel", Modell = "Zafira", Farbe = "rot", PS = 140 },
+            new Auto("autos", Guid.NewGuid()) { Marke = "BMW", Modell = "3er", Farbe = "weiß", PS = 200 },
+            new Auto("autos", Guid.NewGuid()) { Marke = "Mercedes", Modell = "E-Klasse", Farbe = "schwarz", PS = 170 }
         };
+
+        private static IList<Person> persons = new List<Person>() {
+            new Person("Gans", "Gustav") { Adress = "Gänseweg", City = "Entenhausen" },
+            new Person("Beckenbauer", "Franz") { Adress = "Säbener Straße", City = "München" },
+            new Person("Gottschalk", "Thomas") { Adress = "Malibu" , City = "Kalifornien" },
+            new Person("Krüger", "Mike") { Adress = "Piratensender", City = "Powerplay" }
+        };
+
+        private static CloudTableClient tableClient;
 
         static void Main(string[] args)
         {
             var storageCredentials = new StorageCredentials(accountName, keyValue);
             var storageAccount = new CloudStorageAccount(storageCredentials, true);
 
-            var tableClient = storageAccount.CreateCloudTableClient();
+            tableClient = storageAccount.CreateCloudTableClient();
 
+            Autos();
+
+            //Persons();
+
+            var tables = tableClient.ListTables();
+
+            Console.ReadLine();
+        }
+
+        private static void Autos()
+        {
             CloudTable autoTable = tableClient.GetTableReference("autos");
             autoTable.CreateIfNotExists();
 
@@ -41,20 +61,54 @@ namespace storage_table
 
             autoTable.ExecuteBatch(batchOperation);
 
-            var tables = tableClient.ListTables();
+            // single line
+            var insertItem = new Auto("autos", Guid.NewGuid()) { Marke = "Pontiac", Modell = "Firebird", Farbe = "schwarz", PS = 1000 };
+            var insertOpertation = TableOperation.Insert(insertItem);
+            autoTable.Execute(insertOpertation);
+        }
+
+        private static void Persons()
+        {
+            CloudTable personTable = tableClient.GetTableReference("persons");
+            personTable.CreateIfNotExists();
+
+            var batchOperation = new TableBatchOperation();
+            foreach (var item in persons)
+            {
+                batchOperation.Insert(item);
+            }
+
+            personTable.ExecuteBatch(batchOperation);
         }
     }
 
     public class Auto : TableEntity
     {
-        public Auto(string marke, string modell)
+        public Auto(string partition, Guid id)
         {
-            this.PartitionKey = marke;
-            this.RowKey = modell;
+            this.PartitionKey = partition;
+            this.RowKey = id.ToString();
         }
+
+        public string Marke { get; set; }
+
+        public string Modell { get; set; }
 
         public string Farbe { get; set; }
 
         public int PS { get; set; }
+    }
+
+    public class Person : TableEntity
+    {
+        public Person(string lastname, string firstname)
+        {
+            this.PartitionKey = lastname;
+            this.RowKey = firstname;
+        }
+
+        public string Adress { get; set; }
+
+        public string City { get; set; }
     }
 }
